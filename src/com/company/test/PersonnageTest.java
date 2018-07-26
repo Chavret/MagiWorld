604d1e7cd260e4342603de5c91d15c6895045e2c
geoffrey glorian <glorian.geoffrey@gmail.com>
2018-07-26 15:30:24 +0200

    gestion exception cara superieur au niveau

--- src/com/company/test/PersonnageTest.java
+++ src/com/company/test/PersonnageTest.java
@@ -1,7 +1,14 @@
 package com.company.test;
 
+import com.company.Personnage;
+import com.company.exceptions.SommeCaracteristiqueSuperieurAuNiveau;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonnageTest {
-
+    @Test
+    public void Given_CaractereWithTooManyStats_Then_ThrowException() {
+        assertThrows(SommeCaracteristiqueSuperieurAuNiveau.class, () -> new Personnage(2, 1, 1, 1, 0));
+    }
 }
